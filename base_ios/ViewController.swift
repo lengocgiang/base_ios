c06a27444135b331cf87ec7eb947d82fb4e0136f
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2017-10-17 16:18:49 +0700

    Initial Commit

---
+++ base_ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  base_ios
+//
+//  Created by Giang Le Ngoc on 10/17/17.
+//  Copyright © 2017 Giang Le Ngoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
